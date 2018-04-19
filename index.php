7db5de91afe9c1362aadb18106dbbcc151c0008d
thomas <brownaformac@gmail.com>
2018-04-19 17:12:32 +0200

    premiere page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Escape Game - La NASA</title>
+        <link rel="stylesheet" href="librairie/bulma/css/bulma.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <section class="hero is-dark is-fullheight has-text-centered">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        ESCAPE GAME
+                    </h1>
+                    <h2 class="subtitle">
+                        - By La NASA -
+                    </h2>
+                    <a href="#" class="button is-primary">Jouer</a>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
